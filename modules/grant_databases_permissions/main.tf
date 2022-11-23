3ea3134369cb17754f5066c9b0b1480f4558632c
David Pacheco <dpachecomarroyo@gmail.com>
2022-11-23 13:21:39 +0100

    Add schema to postgresql_grant

--- modules/grant_databases_permissions/main.tf
+++ modules/grant_databases_permissions/main.tf
@@ -18,21 +18,25 @@ locals {
   ]
 }
 
-# resource "postgresql_grant" "databases" {
-#   for_each = { for role_definition in local.roles_definition : role_definition.index => role_definition }
-
-#   database    = each.value.database
-#   role        = each.value.role
-#   object_type = "database"
-#   privileges  = each.value.privileges
-# }
-
+# postgresql_grant apply privileges only on existing objects, because this is how GRANT works in Postgres.
 resource postgresql_grant "tables" {
   for_each = { for role_definition in local.roles_definition : role_definition.index => role_definition }
 
   database    = each.value.database
   role        = each.value.role
-  # schema      = postgresql_schema.new_schema.name
+  schema      = lookup(each.value, "schema", "public") #Default scheme
   object_type = "table"
   privileges  = each.value.privileges
 }
+
+# default privileges allows to define privileges that will be applied on newly created objects.
+# resource "postgresql_default_privileges" "tables" {
+#   for_each = { for role_definition in local.roles_definition : role_definition.index => role_definition }
+
+#   database    = each.value.database
+#   role        = each.value.role
+#   schema      = lookup(each.value, "schema", "public") #Default scheme
+#   owner       = lookup(each.value, "owner", replace(each.value.database, "_", "-"))
+#   object_type = "table"
+#   privileges  = each.value.privileges
+# }
