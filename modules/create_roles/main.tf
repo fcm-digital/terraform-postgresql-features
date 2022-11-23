e40f65363d953671796374f668cbb8b87f9c3e6c
David Pacheco <dpachecomarroyo@gmail.com>
2022-11-23 15:03:39 +0100

    Add create_roles module

---
+++ modules/create_roles/main.tf
@@ -0,0 +1,27 @@
+
+resource "random_password" "user-password" {
+  for_each = { for role in var.roles_list : role.name => role if length(lookup(role, "password", "")) == 0}
+
+  keepers = {
+    name = each.key
+  }
+
+  length     = 32
+  special    = false
+}
+
+resource "postgresql_role" "role" {
+  for_each = { for role in var.roles_list : role.name => role }
+
+  name             = each.key
+  superuser        = lookup(each.value, "superuser", false)
+  create_database  = lookup(each.value, "create_database", false)
+  create_role      = lookup(each.value, "create_role", false)
+
+  login            = lookup(each.value, "login", true)
+  password         = lookup(each.value, "password", random_password-user-password[each.key].result)
+  connection_limit = lookup(each.value, "connection_limit", -1)
+  roles            = lookup(each.value, "roles", [])
+
+  depends_on = [random_password.user-password]
+}
---
+++ modules/create_roles/variables.tf
@@ -0,0 +1,17 @@
+
+variable "roles_list" {
+  description = "All Roles definition."
+  type        = list(object({
+    name             = string
+    superuser        = optional(bool)
+    create_database  = optional(bool)
+    create_role      = optional(bool)
+
+    login            = optional(bool)
+    password         = optional(string)
+    connection_limit = optional(number)
+    roles            = optional(List[string])
+  }))
+  default     = []
+  sensitive   = true
+}
