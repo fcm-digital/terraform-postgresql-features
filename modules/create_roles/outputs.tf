631cf527b5efea960c9fe6f66fe7223853f31e42
David Pacheco <dpachecomarroyo@gmail.com>
2022-12-01 12:38:28 +0100

    Fix output naming

--- modules/create_roles/outputs.tf
+++ modules/create_roles/outputs.tf
@@ -3,6 +3,6 @@ output "roles_created"{
     value = values(postgresql_role.role)[*].name
 }
 
-output "roles_passwords" {
+output "roles_password" {
     value = values(random_password.user-password)[*].result
 }
