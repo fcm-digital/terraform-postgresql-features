bac9ddad98ced031b3c6e850eb420a38d5dd75a5
David Pacheco <David.Pacheco@fcm.digital>
2023-03-08 09:35:50 +0100

    remove grant module

--- modules/grant/variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "roles_databases_list" {
-  description = "All Roles that will take specific permissions for the Database."
-  type        = list(object({
-    role     = string
-    database = string 
-  }))
-  default     = []
-}
