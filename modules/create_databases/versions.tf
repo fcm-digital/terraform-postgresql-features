f0f1d66c1e5842e321a073f31e8f77ec9bf3414c
David Pacheco <David.Pacheco@fcm.digital>
2023-07-06 11:51:00 +0200

    Remove 1password provider

--- modules/create_databases/versions.tf
+++ modules/create_databases/versions.tf
@@ -4,9 +4,5 @@ terraform {
       source  = "cyrilgdn/postgresql"
       version = "1.17.1"
     }
-    onepassword = {
-      source = "1Password/onepassword"
-      version = "~> 1.1.2"
-    }
   }
 }
